73b7bf51239fd18320a90e79885ee323f4c210e4
Andrew Gaurin <gaurin1107@gmail.com>
2019-01-24 15:53:01 -0600

    initial cheerios parsing

--- routes/scraper.js
+++ routes/scraper.js
@@ -5,61 +5,81 @@ const cheerio = require('cheerio');
 
 router.get('/', (req, res) => {
 
-  axios.get("http://www.echojs.com/")
+  axios.get("https://boardgamegeek.com/blog/1/")
 
-    .then(handleResponseData)
-    .then((data) => res.json(data))
+    .then(checkResponseData)
+    .then(parseHTMLData)
+    .then(json => res.json(json))
 
     .catch(error => {
-      console.error("Axios Error:\n\t", error, "\n\n");
+      console.error("Scrape Error:\n\t", error, "\n\n");
       logError(error);
       res.status(500).send("Internal Scraping Error");
     })
 
-  ; // axios.get("http://www.echojs.com/")
+    ; // axios.get("http://www.echojs.com/")
 
-}); 
+});
 
 module.exports = router;
 
 
-const handleResponseData = ({ data }={}) => {
+const checkResponseData = ({ data } = {}) => {
   if (data)
     return data;
 
   throw { message: "response had no data!?", data };
-
-   /* // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
-
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
-    });
-
-    // Send a message to the client
-    res.send("Scrape Complete"); */
 }
+
+const parseHTMLData = data => {
+  const $ = cheerio.load(data);
+
+  // // Now, we grab every h2 within an article tag, and do the following:
+  const results = $("#blog_posts .blog_post").map((_index, element) => {
+
+    const title = $(element).find(".post_header .post_title").text().trim();
+    // <div class="post_date">
+		// 	<span class="post_day">23</span>
+		// 	<span class="post_month">Jan</span>
+		// 	<span class="post_year">2019</span>
+    // </div>
+    
+    
+
+    console.log("title:\n", title, title.length);
+
+    return {
+      title
+    }
+    /*  // Save an empty result object
+     var result = {};
+
+     // Add the text and href of every link, and save them as properties of the result object
+     result.title = $(this)
+       .children("a")
+       .text();
+     result.link = $(this)
+       .children("a")
+       .attr("href"); */
+
+    // Create a new Article using the `result` object built from scraping
+    /* db.Article.create(result)
+      .then(function(dbArticle) {
+        // View the added result in the console
+        console.log(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, log it
+        console.log(err);
+      }); */
+
+  });
+
+  console.log(typeof results.get(), Array.isArray(results.get()) && "is Array", results.get().length);
+
+  return data;
+}
+
 
 const logError = (error) => {
 
