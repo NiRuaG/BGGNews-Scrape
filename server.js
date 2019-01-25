351c21e0cb48fb788fb544cadd373b2cec8e56e2
NiRuaG <gaurin1107@gmail.com>
2019-01-25 00:00:31 -0600

    comments & organize

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
 const express = require('express');
 const app = express();
 
+
 const db = require('./config/mongoose');
 
 
-// Middleware
-app.use(require('morgan')('dev'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+{ //* Middleware
+  app.use(require('morgan')('dev'));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+}
 
 
-// Handlebars = View Engine
-app.engine('handlebars',
-  require('express-handlebars')({ defaultLayout: 'main' }));
-app.set("view engine", 'handlebars');
+{ //* Handlebars = View Engine
+  app.engine('handlebars',
+    require('express-handlebars')({ defaultLayout: 'main' }));
+  app.set("view engine", 'handlebars');
+}
 
 
+{ //* Routes
+  app.use('/scrape', require('./routes/scraper'));
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+  app.get('/', (req, res) => {
+    res.render('index');
+  });
+}
 
 
-app.use('/scrape', require('./routes/scraper'));
-
-
-{
+{ //* Star & Listen
   const PORT = process.env.PORT || 3000;
 
   app.listen(PORT, () => {
