3ccb1d64bba9ba95e829591ed367f16dd45a27e3
NiRuaG <gaurin1107@gmail.com>
2019-01-25 23:25:56 -0600

    options to avoid depecration warnings

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/bggnewsscraper";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 module.exports = mongoose;
