efc92840be3c7f6e4fe93272c95d7322231ebe3d
NiRuaG <gaurin1107@gmail.com>
2019-01-27 11:03:41 -0600

    additonal criteria for `body`

--- models/Note.js
+++ models/Note.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
 
 const NoteSchema = new Schema({
-  body: String
+  body: {
+    type: String,
+    required: true,
+    default: "",
+    trim: true
+  },
 });
 
 
